f2c111a846800ea899aa0d070fa71ef3a009fce7
Jared Champayne <j4champa@gmail.com>
2023-05-24 17:05:52 +0200

    updated restaurants and reivews - not styled

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require 'faker'
+categories = ['chinese', 'italian', 'japanese', 'french', 'belgian']
+
+puts 'Creating 5 fake restaurants...'
+5.times do
+  restaurant = Restaurant.new(
+    name:         Faker::Company.name,
+    address:      "#{Faker::Address.street_address}, #{Faker::Address.city}",
+    phone_number: Faker::PhoneNumber.cell_phone,
+    category:     categories.sample
+  )
+  restaurant.save!
+end
+puts 'Finished!'
